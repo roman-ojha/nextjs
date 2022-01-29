907c8c3c2e5af4606991f283c87f6a8e54cef295
Roman Ojha <razzroman98@gmail.com>
2022-01-29 16:46:22 +0545

    next js introduction class

---
+++ Class/Class_01_Introduction_to_Next_JS/inex.js
@@ -0,0 +1,13 @@
+/*
+    *) What is next js?
+        -> React Framework for production
+        -> Framework: own evnironent with a lot of inbuild features to make it easy for use to build super fast web apps. (Ex. File Based Routing)
+        -> Next.js help us to create a production ready apps.
+        -> Next.js is a 'React framework' that enables you to build 'superfast' and 'extremely user-friendly' static websites, as well as full fledge production ready web applications using react.
+        -> In fact, thanks to 'Automatic Static Optimization', "static" and "dyanmic" become one now.
+        -> This feature allow Next.js to build 'hybrid applications' that contain both server-rendered and statically generated pages
+    
+    *) Why Next.JS?
+        -> This big reason to choose Next.js is its SEO efficiency
+        -> it is useing Server-Side Rendering (SSR) and at the same time it can be also a great Static Site Generator (SSG)
+*/
