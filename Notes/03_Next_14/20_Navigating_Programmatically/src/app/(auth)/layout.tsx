d425fbb45379a13ccdbba396565520d8b0088be5
Roman-Ojha <razzroman98@gmail.com>
2023-12-27 22:15:20 +0545

    feat(next-14): navigating programatically

---
+++ Notes/03_Next_14/20_Navigating_Programmatically/src/app/(auth)/layout.tsx
@@ -0,0 +1,44 @@
+"use client";
+import "./styles.css";
+import Link from "next/link";
+
+import { usePathname } from "next/navigation";
+import { useState } from "react";
+
+const navLinks = [
+  { name: "Register", href: "/register" },
+  { name: "Login", href: "/login" },
+  { name: "Forgot Password", href: "/forgot-password" },
+];
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  // https://nextjs.org/docs/app/api-reference/functions/use-pathname
+  const pathname = usePathname();
+  const [input, setInput] = useState("");
+
+  return (
+    <div>
+      <div>
+        <input value={input} onChange={(e) => setInput(e.target.value)} />
+      </div>
+      {navLinks.map((link) => {
+        const isActive = pathname.startsWith(link.href);
+
+        return (
+          <Link
+            className={isActive ? "font-bold mr-4" : "text-blue-500 mr-4"}
+            href={link.href}
+            key={link.name}
+          >
+            {link.name}
+          </Link>
+        );
+      })}
+      {children}
+    </div>
+  );
+}
