9afe60cf4b6dbba0689425a6a3e6938616fd33b3
Roman-Ojha <razzroman98@gmail.com>
2023-12-27 22:07:22 +0545

    feat(next-14): style active link

---
+++ Notes/03_Next_14/19_Active_Links/src/app/products/[productId]/reviews/page.tsx
@@ -0,0 +1,10 @@
+export default function ProductReview() {
+  return (
+    <>
+      <h1>Product Review</h1>
+      <h2>Review 1</h2>
+      <h2>Review 2</h2>
+      <h2>Review 3</h2>
+    </>
+  );
+}
