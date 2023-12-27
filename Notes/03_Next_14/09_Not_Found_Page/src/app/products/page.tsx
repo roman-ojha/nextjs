95c64a6343f2ece8cb6f2c104c328977d2904513
Roman-Ojha <razzroman98@gmail.com>
2023-12-27 16:58:34 +0545

    feat(next-14): added not found page

---
+++ Notes/03_Next_14/09_Not_Found_Page/src/app/products/page.tsx
@@ -0,0 +1,10 @@
+export default function Products() {
+  return (
+    <>
+      <h1>Product List</h1>
+      <h1>Product 1</h1>
+      <h1>Product 2</h1>
+      <h1>Product 3</h1>
+    </>
+  );
+}
