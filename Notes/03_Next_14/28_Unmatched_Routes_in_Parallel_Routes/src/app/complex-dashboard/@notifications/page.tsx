1d208d31fd0dea726c623cf5c0762828db237fad
Roman-Ojha <80277272+roman-ojha@users.noreply.github.com>
2024-04-26 12:03:35 +0545

    feat(next-14): init unmatched routes again

--- Notes/03_Next_14/28_Unmatched_Routes_in_Parallel_Routes/src/app/complex-dashboard/@notifications/page.tsx
+++
@@ -1,8 +0,0 @@
-import Card from "@/components/card";
-import type { NextPage } from "next";
-
-const Notification: NextPage = () => {
-  return <Card>Notification</Card>;
-};
-
-export default Notification;
