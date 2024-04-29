4d75264c3009d7cd9f622bacccf64311ee47b6be
Roman-Ojha <80277272+roman-ojha@users.noreply.github.com>
2024-04-29 22:56:01 +0545

    feat(next-14): completed intercepting routes

---
+++ Notes/03_Next_14/31_Intercepting_Routes/src/app/f1/(.)f2/page.tsx
@@ -0,0 +1,5 @@
+const InterceptedF2 = () => {
+  return <h1>(.) Intercepted F2 Page</h1>;
+};
+
+export default InterceptedF2;
---
+++ Notes/03_Next_14/31_Intercepting_Routes/src/app/f1/f4/(..)f3/page.tsx
@@ -0,0 +1,3 @@
+export default function InterceptingF3() {
+  return <h1>(..) Intercepting F3 page</h1>;
+}
