cbfab517061a359aaa4c365ab991295ae25a7774
Roman-Ojha <80277272+roman-ojha@users.noreply.github.com>
2024-05-03 12:28:42 +0545

    feat(next-14): completed DELETE request

--- Notes/03_Next_14/38_DELETE_Request/src/app/comments/[id]/route.ts
+++ Notes/03_Next_14/38_DELETE_Request/src/app/comments/[id]/route.ts
@@ -23,3 +23,15 @@ export async function PATCH(
 
   return Response.json(comments[index]);
 }
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const index = comments.findIndex(
+    (comment) => comment.id === parseInt(params.id)
+  );
+  const deletedComment = comments[index];
+  comments.splice(index, 1);
+  return Response.json(deletedComment);
+}
