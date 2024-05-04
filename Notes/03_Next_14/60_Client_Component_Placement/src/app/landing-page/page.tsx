accc3c737dfad8099bbded6a7cb7468e4a5f0558
Roman-Ojha <80277272+roman-ojha@users.noreply.github.com>
2024-05-04 12:18:13 +0545

    feat(next-14): understood about client component placement in component tree

---
+++ Notes/03_Next_14/60_Client_Component_Placement/src/app/landing-page/page.tsx
@@ -0,0 +1,13 @@
+import { Navbar } from "@/components/navbar";
+// server component
+
+export default function LandingPage() {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <h1>Page heading</h1>
+      </main>
+    </>
+  );
+}
